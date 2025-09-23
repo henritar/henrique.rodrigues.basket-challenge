2117ddec8b2f9d7daf38136942789c14a655f72a
Henrique Torres <henriquetorres92@gmail.com>
2025-09-23 14:59:45 -0300

    [improvements] Adjust camera damping and swipe gesture constants

--- Assets/Scripts/Runtime/Shared/Constants/GameConstants.cs
+++ Assets/Scripts/Runtime/Shared/Constants/GameConstants.cs
@@ -12,9 +12,9 @@ namespace Assets.Scripts.Runtime.Shared.Constants
         public readonly static float MaxShotTimeToTarget = 2.0f;
         public readonly static float TimeClampFactor = 4.0f;
         public readonly static float BackClampFactor = 8.0f;
-        public readonly static float MaxSwipeDistance = 500f; 
+        public readonly static float MaxSwipeDistance = 800f; 
         public readonly static float MinSwipeSpeed = 100f;
-        public readonly static float SwipeTimeWindow = 1f;
+        public readonly static float SwipeTimeWindow = 0.8f;
         public const float BallRotationMultiplier = 1.5f;
         public const float MaxAngularVelocity = 12.0f;
         public readonly static Vector3 BallOffset = new Vector3(0.15f, 1.6f, 0.3f);
