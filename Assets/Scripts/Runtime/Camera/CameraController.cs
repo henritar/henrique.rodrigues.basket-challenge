7c12e249b5086bae167ae9d93884a35598e3f307
Henrique Torres <henriquetorres92@gmail.com>
2025-09-23 14:54:42 -0300

    [improvements] Add dynamic camera FOV adjustment for aspect ratio

--- Assets/Scripts/Runtime/Camera/CameraController.cs
+++ Assets/Scripts/Runtime/Camera/CameraController.cs
@@ -7,10 +7,26 @@ namespace Assets.Scripts.Runtime.Camera
     public class CameraController : MonoBehaviour, ICameraController
     {
         [SerializeField] CinemachineVirtualCamera _virtualCamera;
+        [SerializeField] private float baseFOV = 60f;
+        
+        private float baseAspectRatio = 16f / 9f;
 
         public void SetCameraFollowTarget(Transform target)
         {
             _virtualCamera.Follow = target;
+        }
+        void Start()
+        {
+            AdjustFOV();
+        }
+
+        private void AdjustFOV()
+        {
+            float currentAspectRatio = (float)Screen.width / Screen.height;
+
+            float fovAdjustment = (currentAspectRatio / baseAspectRatio);
+
+            _virtualCamera.m_Lens.FieldOfView = baseFOV * (baseAspectRatio / currentAspectRatio);
         }
     }
 }
